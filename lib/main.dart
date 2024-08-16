7a36510f650dc9ac396ec656ad7a2c863ff954b5
Ahmad Mustaffar <ahmadmustaffar@Ahmads-Laptop.mynet>
2024-08-16 15:43:25 +0100

    created a pseudo-chess board

--- lib/main.dart
+++ lib/main.dart
@@ -87,37 +87,16 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("Chris was here."),
-            const Text("Ahmad was here."),
-            const Text("Baber was here."),
-            const Text("Svitlana was here."),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: GridView.count(
+            crossAxisCount: 7,
+            children: List.generate(49, (index) {
+              return Container(
+                width: 48.0,
+                height: 48.0,
+                color: index % 2 == 0 ? Colors.black : Colors.white,
+                //margin: const EdgeInsets.all(0.5),
+              );
+            })),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
