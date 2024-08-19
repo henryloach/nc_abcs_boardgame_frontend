ed99e14a3709bda076f93bf1b3bd441c91952037
Chris Loach <chenryloach@gmail.com>
2024-08-19 19:12:28 +0100

    add == operator overload to ChessPiece class so it can be compared now it's not constant

--- lib/game/chess_piece.dart
+++ lib/game/chess_piece.dart
@@ -4,6 +4,16 @@ class ChessPiece {
   bool hasMoved = false;
 
   ChessPiece(this.type, this.colour);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other is! ChessPiece) return false;
+    return other.type == type && other.colour == colour;
+  }
+
+  @override
+  int get hashCode => type.hashCode ^ colour.hashCode;
 }
 
 enum PieceType {
