5237a109606b320fda382e2a9028e40c608a9578
Chris Loach <chenryloach@gmail.com>
2024-08-21 13:36:12 +0100

    add comments to game file

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -5,7 +5,7 @@ import 'package:nc_abcs_boardgame_frontend/game/rules.dart';
 class Game {
   // Use list for maybe adding more players in future
   // TODO maybe make a player class
-  List<String> players = ["player1", "player2"];
+  // List<String> players = ["player1", "player2"];
 
   List<List<ChessPiece?>> board;
   List<ChessPiece> capturedPieces = [];
@@ -21,6 +21,7 @@ class Game {
             ? GameState.whiteToMove
             : GameState.blackToMove; // this is an "initializer list"
 
+  // get the path to the svg file
   String getAssetPathAtSquare((int, int) square) {
     final (row, column) = square;
     final target = board[row][column];
@@ -31,7 +32,6 @@ class Game {
   // safety of this function relies on args coming from legal move set.
   // This function has no restrictions on what can be moved where.
   void movePiece((int, int) moveFromSquare, (int, int) moveToSquare) {
-
     final (startRow, startColumn) = moveFromSquare;
     final (endRow, endColumn) = moveToSquare;
     final ChessPiece? piece = board[startRow][startColumn];
@@ -55,6 +55,14 @@ class Game {
   }
 
   Set<(int, int)> getLegalMoves((int, int) square, {bool testCheck = true}) {
+    // return no legal moves if the game is over
+    if (gameState == GameState.whiteWin ||
+        gameState == GameState.blackWin ||
+        gameState == GameState.draw) {
+      return {};
+    }
+
+    // get the piece from the board position
     final (row, column) = square;
     final ChessPiece? piece = board[row][column];
 
@@ -64,6 +72,7 @@ class Game {
       bool canRepeat,
       CaptureRule captureRule,
     ) {
+      // create the subSet to add to
       Set<(int, int)> resultSet = {};
 
       final int boardHeight = board.length;
@@ -72,27 +81,33 @@ class Game {
       var (y, x) = square;
       var (dy, dx) = direction;
 
+      // allows black and white pawns to share the same ruleset but move in opposite directions
       if (piece!.colour == PieceColour.white) dy = -dy;
 
       do {
+        // get the coordianted of the considered move
         y = y + dy;
         x = x + dx;
 
+        // board bounds check
         if (y >= boardHeight || y < 0) break;
         if (x >= boardWidth || x < 0) break;
 
+        // get the piece(or null) at the target coordinates
         final ChessPiece? targetPiece = board[y][x];
 
+        // can't move through friendly pieces and can only capture the first opposing piece in the direction of movement
         if (targetPiece != null) {
           if (targetPiece.colour == piece.colour) break;
-          if (captureRule == CaptureRule.moveOnly) break;
+          if (captureRule == CaptureRule.moveOnly) break; // pawns can't take forwards
 
           resultSet.add((y, x));
 
           break;
         } else {
-          if (captureRule == CaptureRule.captureOnly) break;
+          if (captureRule == CaptureRule.captureOnly) break; // pawns can't move diagonaly, only capture 
 
+          // square is available to move if it's empty
           resultSet.add((y, x));
 
           // pawn initial double-move
@@ -107,6 +122,7 @@ class Game {
       return resultSet;
     }
 
+    // return no legal moves if it's not the selected pieces turn to move
     if (piece == null ||
         (gameState == GameState.whiteToMove &&
             piece.colour == PieceColour.black) ||
@@ -115,8 +131,10 @@ class Game {
       return {};
     }
 
+    // create result set to add to
     Set<(int, int)> legalMoves = {};
 
+    // add the subset from each of the pieces moverules using the seek function
     moveRuleMap[piece.type].forEach((moveRule) {
       legalMoves = {
         ...legalMoves,
@@ -124,25 +142,39 @@ class Game {
       };
     });
 
+    // remove moves that would result in placing your own king in check
     if (testCheck) {
       legalMoves.removeWhere((targetSquare) {
         return testMoveForOpposingChecks(square, targetSquare);
       });
     }
 
+    // return the set of legal moves
     return legalMoves;
   }
 
   Set<(int, int)> testBoardForChecks() {
+    // save the inital gamestate to remeber who's turn it was
     GameState initial = gameState;
+
+    // create the result set to add to
     Set<(int, int)> checks = {};
+
+    // loop over every square on the board
     for (var row = 0; row < board.length; row++) {
       for (var column = 0; column < board[0].length; column++) {
+        // get the piece(or null) at each square
         final piece = board[row][column];
+
+        // set the gamestate to that pieces color to allow the getLegalMoves function to work
         gameState = piece != null && piece.colour == PieceColour.white
             ? GameState.whiteToMove
             : GameState.blackToMove;
+
+        // get the legal moves of the piece
         final moves = getLegalMoves((row, column), testCheck: false);
+
+        // if any of the legal moves attack an oppsing king then add that move to the set
         if (moves.any(
           (move) {
             final (y, x) = move;
@@ -158,6 +190,8 @@ class Game {
     }
     // revert to the pre-function gameState
     gameState = initial;
+
+    // return the set of checks
     return checks;
   }
 
@@ -196,13 +230,17 @@ class Game {
   }
 
   Set<(int, int)> getAllActivePlayerLegalMoves() {
+    // create set to add to
     Set<(int, int)> resultSet = {};
+    // loop over every board square
     for (var row = 0; row < board.length; row++) {
       for (var column = 0; column < board[0].length; column++) {
-        final piece = board[row][column];
 
+        // get the piece at that square, continue if square is empty 
+        final piece = board[row][column];
         if (piece == null) continue;
 
+        // add that pieces legal moves to the set if it's colorr matches the active colour
         if (piece.colour == PieceColour.white &&
             gameState == GameState.whiteToMove) {
           resultSet.addAll(getLegalMoves((row, column)));
@@ -217,6 +255,7 @@ class Game {
   }
 
   bool isActivePlayerInCheck() {
+    // get all the checks and return true if the attacked king is of the active palyer
     for (final square in testBoardForChecks()) {
       final (row, column) = square;
       if (board[row][column]!.colour == PieceColour.white &&
@@ -232,6 +271,8 @@ class Game {
   }
 
   void testForWinCondition() {
+    // if active player is in check and can't move they lose
+    // if active player can't move and is not in check then it's stalemate
     if (getAllActivePlayerLegalMoves().isNotEmpty) return;
     if (isActivePlayerInCheck() && gameState == GameState.blackToMove) {
       gameState = GameState.whiteWin;
